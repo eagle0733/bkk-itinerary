1807e54bb36fd264d057b107ce9caba79f673e9c
eagle0733 <eric.60908aa@gmail.com>
2025-09-01 10:51:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,29 @@
-const CACHE_NAME="bkk-itinerary-v2_2-fixed-pwa-v2";
-const ASSETS=[
+const CACHE_NAME = "bkk-itinerary-v2_2-pwa-v6";
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
-self.addEventListener("install",e=>{
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+    caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
-self.addEventListener("activate",e=>{
+
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))).then(()=>self.clients.claim())
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())
   );
 });
-self.addEventListener("fetch",e=>{
-  const url=new URL(e.request.url);
-  if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
   }
 });
